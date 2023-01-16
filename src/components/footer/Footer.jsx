308fdcc1108634eb18b5b3bd09c82c7f6ed35e87
belaymit <2belamit@gmail.com>
2023-01-16 21:56:30 +0300

    added footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/footer.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <div>Todo App</div>
+    <div>
+      Ⓒ2023,
+      {' '}
+      All rights reserved. This Web App is developed under the guidance of Microverse
+    </div>
+  </div>
+);
+
+export default Footer;
